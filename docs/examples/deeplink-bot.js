f46211cf8e056e18932377d8c01282743f87575c
viiprogrammer <m_telega@mail.ru>
2022-06-30 23:40:10 +0300

    Remove docs, examples

--- docs/examples/deeplink-bot.js
+++
@@ -1,6 +0,0 @@
-// https://core.telegram.org/bots#deep-linking
-const Telegraf = require('telegraf')
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Deep link payload: ${ctx.startPayload}`))
-bot.launch()
--- docs/examples/hello-bot-module.js
+++
@@ -1,3 +0,0 @@
-// Modules documentation: https://telegraf.js.org/#/?id=telegraf-modules
-// $> telegraf -t `BOT TOKEN` hello-bot-module.js
-module.exports = ({ reply }) => reply('Hello!')
