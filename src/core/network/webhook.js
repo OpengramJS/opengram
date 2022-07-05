7d126a678e24b16a74baf2624123cdd3b9a48d07
viiprogrammer <m_telega@mail.ru>
2022-07-06 01:46:13 +0300

    crypto.timingSafeEqual for hookPath

--- src/core/network/webhook.js
+++ src/core/network/webhook.js
@@ -1,9 +1,39 @@
 const debug = require('debug')('opengram:webhook')
+const crypto = require('crypto')
+
+/**
+ * Call native "crypto.timingSafeEqual" methods.
+ * All passed values will be converted into strings first.
+ *
+ * Runtime is always corresponding to the length of the first parameter (string
+ * a).
+ * @author Michael Raith
+ * @param {string} a
+ * @param {string} b
+ *
+ * @return {boolean}
+ */
+function timingSafeEqual (a, b) {
+  const strA = String(a)
+  const strB = String(b)
+  const aLen = Buffer.byteLength(strA)
+  const bLen = Buffer.byteLength(strB)
+
+  // Always use length of a to avoid leaking the length. Even if this is a
+  // false positive because one is a prefix of the other, the explicit length
+  // check at the end will catch that.
+  const bufA = Buffer.allocUnsafe(aLen)
+  bufA.write(strA)
+  const bufB = Buffer.allocUnsafe(aLen)
+  bufB.write(strB)
+
+  return crypto.timingSafeEqual(bufA, bufB) && aLen === bLen
+}
 
 module.exports = function (hookPath, updateHandler, errorHandler) {
   return (req, res, next) => {
     debug('Incoming request', req.method, req.url)
-    if (req.method !== 'POST' || req.url !== hookPath) {
+    if (req.method !== 'POST' || !timingSafeEqual(hookPath, req.url)) {
       if (typeof next === 'function') {
         return next()
       }
