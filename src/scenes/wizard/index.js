b9bd7d3a0961e260e83deab56f876bd5ec84b629
viiprogrammer <m_telega@mail.ru>
2022-07-10 16:29:34 +0300

    Rewrite Wizard scenes
    Make WizardScene extends BaseScene
    Remove pointless code
    Call WizardScene::middleware() on enter
    
    Co-Authored-By: Kirill Loskutov <kl@sinker.ru>
    Co-Authored-By: Wojciech Pawlik <23058303+wojpawlik@users.noreply.github.com>

--- src/scenes/wizard/index.js
+++ src/scenes/wizard/index.js
@@ -1,39 +1,31 @@
 const Composer = require('../../composer')
 const WizardContext = require('./context')
+const BaseScene = require('../base')
 const { compose, unwrap } = Composer
 
-class WizardScene extends Composer {
+class WizardScene extends BaseScene {
   constructor (id, options, ...steps) {
-    super()
-    this.id = id
-    this.options = typeof options === 'function'
-      ? { steps: [options, ...steps], leaveHandlers: [] }
-      : { steps, leaveHandlers: [], ...options }
-    this.leaveHandler = compose(this.options.leaveHandlers)
+    let tOptions
+    let tSteps
+    if (typeof options === 'function' || 'middleware' in options) {
+      tOptions = undefined
+      tSteps = [options, ...steps]
+    } else {
+      tOptions = options
+      tSteps = steps
+    }
+    super(id, tOptions)
+    this.steps = tSteps
   }
 
-  set ttl (value) {
-    this.options.ttl = value
-  }
-
-  get ttl () {
-    return this.options.ttl
-  }
-
-  leave (...fns) {
-    this.leaveHandler = compose([this.leaveHandler, ...fns])
-    return this
-  }
-
-  leaveMiddleware () {
-    return this.leaveHandler
+  enterMiddleware () {
+    return Composer.compose([this.enterHandler, this.middleware()])
   }
 
   middleware () {
     return compose([
       (ctx, next) => {
-        const wizard = new WizardContext(ctx, this.options.steps)
-        ctx.wizard = wizard
+        ctx.wizard = new WizardContext(ctx, this.steps)
         return next()
       },
       super.middleware(),
