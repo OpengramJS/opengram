500ab861dbc48147357cc62fe1a0ba1ba57a9894
viiprogrammer <m_telega@mail.ru>
2022-07-10 13:07:44 +0300

    Fix formatHTML to work with HTML-reserved characters
    Co-Authored-By: Kirill Loskutov <kl@sinker.ru>

---
+++ src/core/helpers/escape.js
@@ -0,0 +1,9 @@
+const escapeHTML = (string) => {
+  return string
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+}
+
+module.exports = { escapeHTML }
--- src/markup.js
+++ src/markup.js
@@ -1,3 +1,5 @@
+const { escapeHTML } = require('./core/helpers/escape')
+
 class Markup {
   forceReply (value = true) {
     this.force_reply = value
@@ -222,7 +224,7 @@ class Markup {
         available.splice(index, 1)
       }
 
-      result.push(chars[offset])
+      result.push(escapeHTML(chars[offset]))
 
       while (true) {
         const index = opened.findIndex((entity) => entity.offset + entity.length - 1 === offset)
