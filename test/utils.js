59b60c5653a52670922ad9ea0815ffe2ee0eb7c0
viiprogrammer <m_telega@mail.ru>
2022-07-16 01:40:02 +0300

    Update bot info for createBot

--- test/utils.js
+++ test/utils.js
@@ -2,7 +2,7 @@ const { Opengram } = require('../src/opengram')
 
 function createBot (token, options) {
   const bot = new Opengram(token, options)
-  bot.context.botInfo = { username: 'bot' }
+  bot.context.botInfo = { username: 'bot', first_name: 'Bot', id: 42, is_bot: true }
   return bot
 }
 
