205236cfdf5b78f171854bf264d285226fb8fc59
viiprogrammer <m_telega@mail.ru>
2022-08-11 18:04:28 +0300

    docs(jsdoc): add docs for Composer.cashtag

--- src/composer.js
+++ src/composer.js
@@ -505,6 +505,16 @@ class Composer {
     return this.use(Composer.hashtag(...args))
   }
 
+  /**
+   * Registers some middleware(s) that will only be executed if `hashtag` entity is present in the update
+   *
+   * Shortcut to `Composer.entity('cashtag', ...)`
+   *
+   * This method matches entity in channel post, message and media caption
+   *
+   * @param {MiddlewareFn} args The middleware(s) to register
+   * @return {Composer}
+   */
   cashtag (...args) {
     return this.use(Composer.cashtag(...args))
   }
