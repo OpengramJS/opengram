c0dec08ecebc8e3f9279bb7a7862fd74d6584d26
viiprogrammer <m_telega@mail.ru>
2022-08-15 21:47:32 +0300

    docs(jsdoc): update some types for bots api 6.2
    Add custom_emoji type for entityType
    Add custom_emoji_id field for MessageEntity
    Update description for MessageEntity

--- src/types/telegram.js
+++ src/types/telegram.js
@@ -103,7 +103,7 @@
 /** @typedef {'Markdown'|'MarkdownV2'|'HTML'} parseMode */
 /** @typedef {
    'mention'|'hashtag'|'cashtag'|'bot_command'|'url'|'email'|'phone_number'|'bold'|'italic'|'underline'|'strikethrough'
-   |'spoiler'|'code'|'pre'|'text_link'|'text_mention'
+   |'spoiler'|'code'|'pre'|'text_link'|'text_mention'|'custom_emoji'
   } entityType */
 
 /**
@@ -118,13 +118,15 @@
  *    “strikethrough” (~~strikethrough text~~), “spoiler” (spoiler message),
  *    “code” (`monowidth string`), “pre” (`monowidth block`),
  *    “text_link” (for clickable text URLs), “text_mention”
- *    (for users [without usernames](https://telegram.org/blog/edit#new-mentions))
+ *    (for users [without usernames](https://telegram.org/blog/edit#new-mentions)),
+ *    “custom_emoji” (for inline custom emoji stickers)
  * @property {number} offset Offset in UTF-16 code units to the start of the entity
  * @property {number} length Length of the entity in UTF-16 code units
  * @property {string} [url] *Optional.* For “text_link” only, URL that will be opened after user taps on the text
  * @property {User} [user] *Optional.* For “text_mention” only, the mentioned user
  * @property {string} [language] *Optional.* For “pre” only, the programming language of the entity text
- *
+ * @property {string} [custom_emoji_id] *Optional.* For “custom_emoji” only, unique identifier of the custom emoji.
+ *    Use getCustomEmojiStickers to get full information about the sticker
  */
 
 /**
