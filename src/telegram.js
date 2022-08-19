79393ff0d2111c8a7ffad6462cd79c11a5dfe2af
viiprogrammer <m_telega@mail.ru>
2022-08-19 20:01:30 +0300

    chore: change jsdoc description

--- src/telegram.js
+++ src/telegram.js
@@ -638,7 +638,7 @@ class Telegram extends ApiClient {
   }
 
   /**
-   * Use this method to get up to date information about the chat (current name of the user for one-on-one
+   * Use this method to get up-to-date information about the chat (current name of the user for one-on-one
    * conversations, current username of a user, group or channel, etc.).
    *
    * Returns a {@link Chat} object on success.
