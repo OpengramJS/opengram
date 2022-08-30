8d511a4ac3722ad53ab8d812d0d88f3d518719a7
viiprogrammer <m_telega@mail.ru>
2022-08-30 21:01:50 +0300

    docs(jsdoc): add docs for context.setPassportDataErrors

--- src/context.js
+++ src/context.js
@@ -1154,6 +1154,21 @@ class OpengramContext {
     return this.telegram.getMyDefaultAdministratorRights(forChannels)
   }
 
+  /**
+   * Informs current user that some of the Telegram Passport elements they provided contains errors.
+   * The user will not be able to re-submit their Passport to you until the errors are fixed
+   * (the contents of the field for which you returned the error must change).
+   *
+   * Returns `True` on success.
+   *
+   * Use this if the data submitted by the user doesn't satisfy the standards your service requires for any reason.
+   * For example, if a birthday date seems invalid, a submitted document is blurry, a scan shows evidence of tampering,
+   * etc. Supply some details in the error message to make sure the user knows how to correct the issues.
+   * @see https://core.telegram.org/bots/api#setpassportdataerrors
+   * @param {PassportElementError[]} errors Array describing the errors
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   setPassportDataErrors (errors) {
     this.assert(this.chat, 'setPassportDataErrors')
     return this.telegram.setPassportDataErrors(this.from.id, errors)
