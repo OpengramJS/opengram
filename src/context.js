bb89696cabc8f3d45e743eb63ef726efd0edacc8
viiprogrammer <m_telega@mail.ru>
2022-09-04 15:45:15 +0300

    docs(jsdoc): add docs for context.replyWithChatAction

--- src/context.js
+++ src/context.js
@@ -1447,6 +1447,32 @@ class OpengramContext {
     return this.telegram.stopPoll(this.chat.id, messageId, extra)
   }
 
+  /**
+   * Use this method when you need to tell the user that something is happening on the bot's side.
+   * The status is set for 5 seconds or less (when a message arrives from your bot,
+   * Telegram clients clear its typing status).
+   *
+   * Returns `True` on success.
+   *
+   * Example: The [ImageBot](https://t.me/imagebot) needs some time to process a request and upload the image.
+   * Instead of sending a text message along the lines of “Retrieving image, please wait…”,
+   * the bot may use {@link replyWithChatAction} with `action = upload_photo`. The user will see a “sending photo”
+   * status for the bot.
+   *
+   * We only recommend using this method when a response from the bot will take a **noticeable** amount of time to arrive.
+   * @see https://core.telegram.org/bots/api#sendchataction
+   * @param {Action} action Type of action to broadcast. Choose one, depending on what the user is about to receive:
+   *    `typing` for text messages,
+   *    `upload_photo` for {@link replyWithPhoto photos},
+   *    `record_video` or `upload_video` for {@link replyWithVideo videos},
+   *    `record_voice` or `upload_voice` for {@link replyWithVoice voice} notes,
+   *    `upload_document` for general {@link replyWithDocument files},
+   *    `choose_sticker` for {@link replyWithSticker stickers},
+   *    `find_location` for {@link replyWithLocation location data},
+   *    `record_video_note` or `upload_video_note` for {@link replyWithVideoNote video notes}.
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   replyWithChatAction (action) {
     this.assert(this.chat, 'replyWithChatAction')
     return this.telegram.sendChatAction(this.chat.id, action)
