122dcff7f9e7c4cff34fb7d6caef4d104ba4270f
viiprogrammer <m_telega@mail.ru>
2022-09-04 16:13:06 +0300

    docs(jsdoc): add docs for context.setStickerSetThumb

--- src/context.js
+++ src/context.js
@@ -1587,6 +1587,27 @@ class OpengramContext {
     return this.telegram.setStickerPositionInSet(sticker, position)
   }
 
+  /**
+   * Use this method to set the thumbnail of a sticker set. Animated thumbnails can be set for animated sticker
+   * sets only. Video thumbnails can be set only for video sticker sets only.
+   *
+   * Returns `True` on success.
+   * @see https://core.telegram.org/bots/api#setstickersetthumb
+   * @param {string} name Sticker set name
+   * @param {number} userId User identifier of the sticker set owner
+   * @param {attachmentFile} [thumb] A **PNG** image with the thumbnail, must be up to 128 kilobytes in size and have width and height
+   *    exactly 100px, or a **TGS** animation with the thumbnail up to 32 kilobytes in size;
+   *    see https://core.telegram.org/stickers#animated-sticker-requirements for animated sticker technical requirements,
+   *    or a **WEBM** video with the thumbnail up to 32 kilobytes in size;
+   *    see https://core.telegram.org/stickers#video-sticker-requirements for video sticker technical
+   *    requirements. Pass a file_id as a String to send a file that already exists on the Telegram servers,
+   *    pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one
+   *    using multipart/form-data.
+   *    [More information on Sending Files »](https://core.telegram.org/bots/api#sending-files).
+   *    Animated sticker set thumbnails can't be uploaded via HTTP URL.
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   setStickerSetThumb (name, userId, thumb) {
     return this.telegram.setStickerSetThumb(name, userId, thumb)
   }
