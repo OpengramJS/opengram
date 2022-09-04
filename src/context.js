a56da4f539302ba0de59496e6022a3b44733402e
viiprogrammer <m_telega@mail.ru>
2022-09-04 15:30:13 +0300

    docs(jsdoc): add docs for context.replyWithQuiz

--- src/context.js
+++ src/context.js
@@ -1416,6 +1416,17 @@ class OpengramContext {
     return this.telegram.sendPoll(this.chat.id, question, options, extra)
   }
 
+  /**
+   * Use this method to send a native poll with type `quiz` to chat from current update.
+   *
+   * On success, the sent {@link Message} is returned.
+   * @see https://core.telegram.org/bots/api#sendpoll
+   * @param {string} question Poll question, 1-300 characters
+   * @param {string[]} options List of answer options, 2-10 strings 1-100 characters each
+   * @param {object|Extra} [extra] Other parameters
+   * @throws {TelegramError}
+   * @return {Promise<Message>}
+   */
   replyWithQuiz (question, options, extra) {
     this.assert(this.chat, 'replyWithQuiz')
     return this.telegram.sendQuiz(this.chat.id, question, options, extra)
