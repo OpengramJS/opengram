e432e7666ba4678ece79a0c2e1fc7c72ea25f742
viiprogrammer <m_telega@mail.ru>
2022-09-27 00:16:51 +0300

    docs(jsdoc): add types for context

--- src/types/context.js
+++ src/types/context.js
@@ -3,3 +3,43 @@
  * @property {boolean} channelMode Enable / Disable channel mode, you can find more information about this
  *    in Opengram constructor options type
  */
+
+/**
+ * @typedef {object} ChatMemberUpdated
+ */
+
+/**
+ * @typedef {object} CallbackQuery
+ */
+
+/**
+ * @typedef {object} ChosenInlineResult
+ */
+
+/**
+ * @typedef {object} PreCheckoutQuery
+ */
+
+/**
+ * @typedef {object} ShippingQuery
+ */
+
+/**
+ * @typedef {object} PassportData
+ */
+
+/**
+ * @typedef {object} InlineQuery
+ */
+
+/**
+ * @typedef {object} ChatJoinRequest
+ */
+
+/**
+ * @typedef {object} WebAppData
+ */
+
+/**
+ * @typedef {object} PollAnswer
+ */
