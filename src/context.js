8b0a8381347fae880cd6b7ab095a1b7ed06d5067
viiprogrammer <m_telega@mail.ru>
2022-09-28 17:35:33 +0300

    docs(jsdoc): add docs for context.answerCbQuery

--- src/context.js
+++ src/context.js
@@ -447,6 +447,24 @@ class OpengramContext {
     return this.telegram.answerInlineQuery(this.inlineQuery.id, results, extra)
   }
 
+  /**
+   * Use this method to send answers to current callback query sent from
+   * [inline keyboards](https://core.telegram.org/bots#inline-keyboards-and-on-the-fly-updating).
+   * The answer will be displayed to the user as a notification at the top of the chat screen or as an alert.
+   *
+   * On success, `True` is returned.
+   *
+   * Alternatively, the user can be redirected to the specified Game URL. For this option to work, you must first
+   * create a game for your bot via [@BotFather](https://t.me/BotFather) and accept the terms.
+   * Otherwise, you may use links like `t.me/your_bot?start=XXXX` that open your bot with a parameter.
+   * @see https://core.telegram.org/bots/api#answercallbackquery
+   * @param {string} text Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
+   * @param {boolean} [showAlert] If True, an alert will be shown by the client instead of a notification at the top of the
+   *    chat screen. Defaults to false.
+   * @param {object} [extra] Other parameters
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   answerCbQuery (text, showAlert, extra) {
     this.assert(this.callbackQuery, 'answerCbQuery')
     return this.telegram.answerCbQuery(this.callbackQuery.id, text, showAlert, extra)
