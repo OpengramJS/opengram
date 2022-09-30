0691a6698f620e3bb19735639a14ef17e766e958
viiprogrammer <m_telega@mail.ru>
2022-09-30 20:34:22 +0300

    docs(jsdoc): add docs for context.unpinAllChatMessages

--- src/context.js
+++ src/context.js
@@ -991,6 +991,17 @@ class OpengramContext {
     return this.telegram.unpinChatMessage(this.chat.id, extra)
   }
 
+  /**
+   * Use this method to clear the list of pinned messages in chat from current update.
+   * If the chat is not a private chat, the bot must be an administrator in the chat for this to work and must
+   * have the 'can_pin_messages' administrator right in a supergroup or 'can_edit_messages' administrator
+   * right in a channel.
+   *
+   * Returns `True` on success.
+   * @see https://core.telegram.org/bots/api#unpinallchatmessages
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   unpinAllChatMessages () {
     this.assert(this.chat, 'unpinAllChatMessages')
     return this.telegram.unpinAllChatMessages(this.chat.id)
