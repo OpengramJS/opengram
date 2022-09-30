cbc86a397c0e6aa77f54424f33eb32ab05d718ea
viiprogrammer <m_telega@mail.ru>
2022-09-30 20:31:39 +0300

    docs(jsdoc): add docs for context.pinChatMessage

--- src/context.js
+++ src/context.js
@@ -957,6 +957,18 @@ class OpengramContext {
     return this.telegram.setChatDescription(this.chat.id, description)
   }
 
+  /**
+   * Use this method to add a message to the list of pinned messages in current chat. If the chat is not a private chat,
+   * the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages' administrator
+   * right in a supergroup or 'can_edit_messages' administrator right in a channel.
+   *
+   * Returns `True` on success.
+   * @see https://core.telegram.org/bots/api#pinchatmessage
+   * @param {number} messageId Identifier of a message to pin
+   * @param {object} [extra] Other parameters
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
   pinChatMessage (messageId, extra) {
     this.assert(this.chat, 'pinChatMessage')
     return this.telegram.pinChatMessage(this.chat.id, messageId, extra)
