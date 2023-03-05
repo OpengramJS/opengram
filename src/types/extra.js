e5fd585a48b7de11914de21d8cd6ca99328113bb
viiprogrammer <m_telega@mail.ru>
2023-03-05 15:14:30 +0300

    docs(jsdoc): add extra params type for answerCbQuery

---
+++ src/types/extra.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {object} ExtraAnswerCbQuery
+ * @see https://core.telegram.org/bots/api#answercallbackquery
+ * @property {string} [url] *Optional*. URL that will be opened by the user's client. If you have created a
+ *   [Game](https://core.telegram.org/bots/api#game) and accepted the conditions via
+ *   [@BotFather](https://t.me/botfather), specify the URL that opens your game - note that this will only work if
+ *   the query comes from a [callback_game](https://core.telegram.org/bots/api#inlinekeyboardbutton) button.
+ *   Otherwise, you may use links like `t.me/your_bot?start=XXXX` that open your bot with a parameter.
+ * @property {number} [cache_time] *Optional*. The maximum amount of time in seconds that the result of the callback
+ *   query may be cached client-side. Telegram apps will support caching starting in version `3.14`. Defaults to `0`.
+ */
