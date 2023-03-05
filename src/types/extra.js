93f83794fcd54394852221092347c3217db54584
viiprogrammer <m_telega@mail.ru>
2023-03-05 17:16:30 +0300

    docs(jsdoc): add extra params type for answerInlineQuery

--- src/types/extra.js
+++ src/types/extra.js
@@ -268,3 +268,27 @@
  * @property {MaskPosition} [mask_position] *Optional*. A JSON-serialized object for position where the mask should be
  *   placed on faces
  */
+
+/**
+ * @typedef {object} ExtraAnswerInlineQuery
+ * @see https://core.telegram.org/bots/api#answerinlinequery
+ * @property {number} [cache_time] *Optional*. The maximum amount of time in seconds that the result of the inline
+ *   query may be cached on the server. Defaults to 300.
+ * @property {boolean} [is_personal] *Optional*. Pass *True* if results may be cached on the server side only for the
+ *   user that sent the query. By default, results may be returned to any user who sends the same query
+ * @property {string} [next_offset] *Optional*. Pass the offset that a client should send in the next query with the
+ *   same text to receive more results. Pass an empty string if there are no more results or if you don't support
+ *   pagination. Offset length can't exceed 64 bytes.
+ * @property {string} [switch_pm_text] *Optional*. If passed, clients will display a button with specified text that
+ *   switches the user to a private chat with the bot and sends the bot a start message with the parameter
+ *   *switch_pm_parameter*
+ * @property {string} [switch_pm_parameter] *Optional*.
+ *   [Deep-linking](https://core.telegram.org/bots/features#deep-linking) parameter for the `/start` message sent to the
+ *   bot when user presses the switch button. 1-64 characters, only `A-Z`, `a-z`, `0-9`, `_` and `-` are allowed.
+ *   *Example:* An inline bot that sends YouTube videos can ask the user to connect the bot to their YouTube account
+ *   to adapt search results accordingly. To do this, it displays a 'Connect your YouTube account' button above the
+ *   results, or even before showing any. The user presses the button, switches to a private chat with the bot and,
+ *   in doing so, passes a start parameter that instructs the bot to return an OAuth link. Once done, the bot can offer
+ *   a [*switch_inline*](https://core.telegram.org/bots/api#inlinekeyboardmarkup) button so that the user can easily
+ *   return to the chat where they wanted to use the bot's inline capabilities.
+ */
