e8c4c316caa11a39f5c2f722100ec8d2293e596c
viiprogrammer <m_telega@mail.ru>
2023-03-05 17:46:51 +0300

    docs(jsdoc): add extra params type for sendInvoice

--- src/types/extra.js
+++ src/types/extra.js
@@ -576,3 +576,52 @@
  * @property {boolean} [allow_sending_without_reply] *Optional*. Pass *True* if the message should be sent even if the
  *   specified replied-to message is not found
  */
+
+/**
+ * @typedef {object} ExtraInvoice
+ * @see https://core.telegram.org/bots/api#sendinvoice
+ * @property {number} [message_thread_id] *Optional*. Unique identifier for the target message thread (topic)
+ *   of the forum; for forum supergroups only
+ * @property {string} [start_parameter] *Optional*. Unique deep-linking parameter. If left empty, **forwarded copies**
+ *   of the sent message will have a *Pay* button, allowing multiple users to pay directly from the forwarded message,
+ *   using the same invoice. If non-empty, forwarded copies of the sent message will have a *URL* button with a deep
+ *   link to the bot (instead of a *Pay* button), with the value used as the start parameter
+ * @property {boolean} [is_flexible] *Optional*. Pass *True* if the final price depends on the shipping method
+ * @property {number} [max_tip_amount] *Optional*. The maximum accepted amount for tips in the *smallest units*
+ *   of the currency (integer, **not** float/double). For example, for a maximum tip of `US$ 1.45` pass
+ *   `max_tip_amount = 145`. See the *exp* parameter in
+ *   [currencies.json](https://core.telegram.org/bots/payments/currencies.json), it shows the number of digits past
+ *   the decimal point for each currency (`2` for the majority of currencies). Defaults to `0`
+ * @property {number[]} [suggested_tip_amounts] *Optional*. A JSON-serialized array of suggested amounts of tips in the
+ *   *smallest units* of the currency (integer, **not** float/double). At most 4 suggested tip amounts can be
+ *   specified.
+ *   The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed
+ *   *max_tip_amount*.
+ * @property {object} [provider_data] *Optional*. JSON-serialized data about the invoice, which will be shared with the
+ *   payment provider. A detailed description of required fields should be provided by the payment provider.
+ * @property {string} [photo_url] *Optional*. URL of the product photo for the invoice. Can be a photo of the goods or
+ *   a marketing image for a service. People like it better when they see what they are paying for.
+ * @property {number} [photo_size] *Optional*. Photo size in bytes
+ * @property {number} [photo_width] *Optional*. Photo width
+ * @property {number} [photo_height] *Optional*. Photo height
+ * @property {boolean} [need_name] *Optional*. Pass *True* if you require the user's full name to complete the order
+ * @property {boolean} [need_phone_number] *Optional*. Pass *True* if you require the user's phone number to complete
+ *   the order
+ * @property {boolean} need_email *Optional*. Pass *True* if you require the user's email address to complete the order
+ * @property {boolean} [need_shipping_address] *Optional*. Pass *True* if you require the user's shipping address to
+ *   complete the order
+ * @property {boolean} [send_phone_number_to_provider] *Optional*. Pass *True* if the user's phone number should be
+ *   sent to provider
+ * @property {boolean} [send_email_to_provider] *Optional*. Pass *True* if the user's email address should be sent to
+ *   provider
+ * @property {boolean} [disable_notification] *Optional*. Sends the message
+ *   [silently](https://telegram.org/blog/channels-2-0#silent-messages). Users will receive a notification with no
+ *   sound.
+ * @property {boolean} [protect_content] *Optional*. Protects the contents of the sent message from forwarding
+ * @property {number} [reply_to_message_id] *Optional*. If the message is a reply, ID of the original message
+ * @property {boolean} [allow_sending_without_reply] *Optional*. Pass *True* if the message should be sent even if the
+ *   specified replied-to message is not found
+ * @property {InlineKeyboardMarkup} [reply_markup] *Optional* A JSON-serialized object for an
+ *   [inline keyboard](https://core.telegram.org/bots/features#inline-keyboards). If empty, one 'Pay `total price`'
+ *   button will be shown. If not empty, the first button must be a Pay button.
+ */
