85ea31c0e8cee4950783df3e02800e6bf77d129b
viiprogrammer <m_telega@mail.ru>
2023-03-05 18:19:10 +0300

    docs(jsdoc): add extra params type for promoteChatMember

--- src/types/extra.js
+++ src/types/extra.js
@@ -896,3 +896,32 @@
  * @property {BotCommandScope} [scope] *Optional*. A JSON-serialized object, describing scope of users for which the commands are
  *   relevant. Defaults to [BotCommandScopeDefault](https://core.telegram.org/bots/api#botcommandscopedefault).
  */
+
+/**
+ * @typedef {object} ExtraPromoteChatMember
+ * @see https://core.telegram.org/bots/api#promotechatmember
+ * @property {boolean} [is_anonymous] *Optional*. *True*, if the poll needs to be anonymous, defaults to *True*
+ * @property {boolean} [can_manage_chat] *Optional*.  *True*, if the administrator can access the chat event log, chat
+ *   statistics, message statistics in channels, see channel members, see anonymous
+ *   administrators in supergroups and ignore slow mode. Implied by any other
+ *   administrator privilege
+ * @property {boolean} [can_post_messages] *Optional*. *True*, if the administrator can post in the channel; channels
+ *   only
+ * @property {boolean} [can_edit_messages] *Optional*. *True*, if the administrator can edit messages of other users
+ *   and can pin messages; channels only
+ * @property {boolean} [can_delete_messages] *Optional*. *True*, if the administrator can delete messages of other users
+ * @property {boolean} [can_manage_video_chats] *Optional*. *True*, if the administrator can manage video chats
+ * @property {boolean} [can_restrict_members] *Optional*. *True*, if the administrator can restrict, ban or unban chat
+ *   members
+ * @property {boolean} [can_promote_members] *Optional*. *True*, if the administrator can add new administrators with
+ *   a subset of
+ *   their own privileges or demote administrators that they have promoted, directly or indirectly (promoted by
+ *   administrators that were appointed by the user)
+ * @property {boolean} [can_invite_users] *Optional*. *True*, if the user is allowed to invite new users to the chat
+ * @property {boolean} [can_change_info] *Optional*. *True*, if the user is allowed to change the chat title, photo
+ *   and other settings
+ * @property {boolean} [can_pin_messages] *Optional*. *True*, if the user is allowed to pin messages; groups and
+ *   supergroups only
+ * @property {boolean} [can_manage_topics] *Optional*. *True*, if the user is allowed to create, rename, close, and
+ *   reopen forum topics; supergroups only
+ */
