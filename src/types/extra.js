f7dab07e538b918a17a9d746c62caeca58fcfa7f
viiprogrammer <m_telega@mail.ru>
2023-03-05 15:34:29 +0300

    docs(jsdoc): add extra params type for sendMessage

--- src/types/extra.js
+++ src/types/extra.js
@@ -140,3 +140,29 @@
  *   [custom reply keyboard](https://core.telegram.org/bots/features#keyboards), instructions to remove reply keyboard
  *   or to force a reply from the user.
  */
+
+/**
+ * @typedef {object} ExtraSendMessage
+ * @see https://core.telegram.org/bots/api#sendmessage
+ * @property {number} [message_thread_id] *Optional*. Unique identifier of a message thread to which the message
+ *   belongs; for supergroups only
+ * @property {string} [parse_mode] *Optional*. Mode for parsing entities in the photo caption. See [formatting
+ *   options](https://core.telegram.org/bots/api/#formatting-options) for more
+ *   details.
+ * @property {Array<MessageEntity>} [entities] *Optional*. A JSON-serialized list of special entities that appear in the
+ *   new caption, which can be specified instead of `parse_mode`
+ * @property {boolean} [disable_web_page_preview] *Optional*. Disables link previews for links in this message
+ * @property {boolean} [disable_notification] *Optional*. Sends the message
+ *   [silently](https://telegram.org/blog/channels-2-0#silent-messages). Users will receive
+ *   a notification with no sound.
+ * @property {boolean} [protect_content] *Optional*. Protects the contents of the forwarded message from forwarding and
+ *   saving
+ * @property {number} [reply_to_message_id] *Optional*. If the message is a reply, ID of the original message
+ * @property {boolean} [allow_sending_without_reply] *Optional*. Pass `True`, if the message should be sent even if the
+ *   specified replied-to message is not found
+ * @property {ForceReply|ReplyKeyboardRemove|ReplyKeyboardMarkup|InlineKeyboardMarkup} [reply_markup] *Optional*.
+ *   Additional interface options. A JSON-serialized object for an
+ *   [inline keyboard](https://core.telegram.org/bots/features#inline-keyboards),
+ *   [custom reply keyboard](https://core.telegram.org/bots/features#keyboards), instructions to remove reply keyboard
+ *   or to force a reply from the user.
+ */
