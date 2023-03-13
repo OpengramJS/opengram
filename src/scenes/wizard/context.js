89b965846f8f8112b3ada737de40bb32959b3cad
viiprogrammer <m_telega@mail.ru>
2023-03-14 00:28:44 +0300

    docs(jsdoc): add description / types for WizardContext methods

--- src/scenes/wizard/context.js
+++ src/scenes/wizard/context.js
@@ -3,6 +3,10 @@
  * @memberof Scenes
  */
 class WizardContext {
+  /**
+   * @param {OpengramContext} ctx Context
+   * @param {MiddlewareFn[]} steps Steps
+   */
   constructor (ctx, steps) {
     this.ctx = ctx
     this.steps = steps
@@ -14,16 +18,32 @@ class WizardContext {
     return this.cursor >= 0 && this.steps[this.cursor]
   }
 
+  /**
+   * Selects step of wizard
+   *
+   * @param {number} index Step index, starting from `0` (zero)
+   * @return {WizardContext}
+  */
   selectStep (index) {
     this.cursor = index
     this.ctx.scene.session.cursor = index
     return this
   }
 
+  /**
+   * Increments step of wizard
+   *
+   * @return {WizardContext}
+   */
   next () {
     return this.selectStep(this.cursor + 1)
   }
 
+  /**
+   * Decrements step of wizard
+   *
+   * @return {WizardContext}
+   */
   back () {
     return this.selectStep(this.cursor - 1)
   }
