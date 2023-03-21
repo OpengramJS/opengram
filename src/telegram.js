796e605d8fed4673f0696c45a0a31d4278c132e4
viiprogrammer <m_telega@mail.ru>
2023-03-21 03:04:16 +0300

    feat: add setMyDescription (Bots API 6.6)

--- src/telegram.js
+++ src/telegram.js
@@ -1918,6 +1918,23 @@ class Telegram extends ApiClient {
   }
 
   /**
+   * Use this method to change the bot's description, which is shown in the chat with the bot if the chat is empty.
+   *
+   * Returns `True` on success.
+   *
+   * @see https://core.telegram.org/bots/api#setmydescription
+   * @param {string} [description] New bot description; 0-512 characters. Pass an empty string to remove the dedicated
+   *   description for the given language.
+   * @param {string} [languageCode] A two-letter ISO 639-1 language code. If empty, the description will be applied to all users
+   *   for whose language there is no dedicated description.
+   * @throws {TelegramError}
+   * @return {Promise<boolean>}
+   */
+  setMyDescription (description, languageCode) {
+    return this.callApi('deleteMyCommands', { description, language_code: languageCode })
+  }
+
+  /**
    * Use this method to delete the list of the bot's commands for the given scope and user language.
    * After deletion, higher level commands will be shown to affected users.
    *
